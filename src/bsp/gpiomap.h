5109cb3f716706dfa955c3e3087a5c6d41ebfb2a
SpaceBaker <emile.forcier@gmail.com>
2025-03-16 23:42:33 -0400

    feat: created a macro to "get" GPIO_ODR__OD_xx with PIN define
    Exemple : ODR_PIN(15) -> GPIO_ODR_OD15

--- src/bsp/gpiomap.h
+++ src/bsp/gpiomap.h
@@ -5,14 +5,18 @@
 #include "stm32l4xx.h"
 
 
+#define _CONCAT(x,y)    x ## y
+#define CONCAT(x,y)     _CONCAT(x, y)
+
 /* Serial Comm */
 #define UART_PORT (GPIOA)
 #define UART_TX_PIN  (0)
 #define UART_RX_PIN  (1)
 
 /* LED */
-#define LED_PORT (GPIOB)
-#define LED_PIN  (14)
+#define ODR_PIN(pin) CONCAT(GPIO_ODR_OD, pin)
+#define LED_PORT GPIOB
+#define LED_PIN  14
 
 
 #endif /* GPIOMAP_H */
